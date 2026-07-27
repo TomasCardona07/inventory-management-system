db76ad055b158ed60bce69753161d67a94f6417f
Tomas <tomascardona787@gmail.com>
2026-07-27 01:06:52 -0500

    Modulando services

--- src/main/java/services/InventarioService.java
+++ src/main/java/services/InventarioService.java
@@ -1,12 +1,12 @@
 package services;
 import util.*;
-import model.*;
 import repository.*;
 import java.util.*;
 
 public class InventarioService {
     Registros registros = new Registros();
     Reportes reportes = new Reportes();
+    Movimientos movimientos = new Movimientos();
     InputValidator inputValidator = new InputValidator();
 
     // ======== IMPORTANDO COLECCIÓNES DESDE REPOSITORY ==========
@@ -109,110 +109,26 @@ public class InventarioService {
     
     // ============= CASE 1: MOSTRAR TOTAL DE ENTRADAS REGISTRADAS  ============ 
     public void entradasRegistradas(){
-        Map<String,Producto> productos = producto.getProductos();
-        int sumaEntradas = 0;
-        if (!movimiento.arrayVacio()) {
-            for (Producto producto : productos.values()) {
-                sumaEntradas += producto.getContadorEntradas();
-            }
-            System.out.println("ENTRADAS REGISTRADAS: " + sumaEntradas);
-        }
-        else{
-            System.err.println("NO HAY MOVIMIENTOS REGISTRADOS");
-        }
+        movimientos.entradasRegistradas();
     }
 
     // ============= CASE 2: MOSTRAR TOTAL DE SALIDAS REGISTRADAS  ============ 
     public void salidasRegistradas(){
-        Map<String,Producto> productos = producto.getProductos();
-        int sumaSalidas = 0;
-        if (!movimiento.arrayVacio()) {
-            for (Producto producto : productos.values()) {
-                sumaSalidas += producto.getContadorSalidas();
-            }
-            System.out.println("SALIDAS REGISTRADAS: " + sumaSalidas);
-        }
-        else{
-            System.err.println("NO HAY MOVIMIENTOS REGISTRADOS");
-        }
+        movimientos.salidasRegistradas();
     }
 
     // ============= CASE 3: MOSTRAR ULTIMOS MOVIMIENTOS REGISTRADOS ============ 
     public void ultimosMovimientos(){
-        ArrayList<Movimiento> movimientos = movimiento.getMovimientos();
-        if (!movimiento.arrayVacio()) {
-            try {
-                for (int i = movimientos.size()-1; i >= movimientos.size()-5; i--){
-                    if (!movimientos.get(i).getTipoMovimiento().equalsIgnoreCase("null")) {
-                        System.out.println("TIPO DE MOVIMIENTO: " + movimientos.get(i).getTipoMovimiento());
-                        System.out.println("CODIGO DEL PRODUCTO: " + movimientos.get(i).getCodigoProducto());
-                        if (movimientos.get(i).getTipoMovimiento().equalsIgnoreCase("ENTRADA")) {
-                            System.out.println("CANTIDAD INGRESADA: " + movimientos.get(i).getCantidad());
-                        }
-                        else{
-                            System.out.println("CANTIDAD EXPORTADA: " + movimientos.get(i).getCantidad());
-                        }
-                        System.out.println("========================");
-                    }
-                }
-            }catch (IndexOutOfBoundsException e) {
-                System.out.println("ESTOS SON LOS ULTIMOS MOVIMIENTOS REGISTRADOS :)");
-            }
-        }
-        else{
-            System.err.println("NO HAY MOVIMIENTOS REGISTRADOS");
-        }
+        movimientos.ultimosMovimientos();
     }
 
     // ============= CASE 4: MOSTRAR PRODUCTOS QUE NUNCA HAN TENIDO ENTRADAS ============ 
     public void productosSinEntradas(){
-        Map<String,Producto> productos = producto.getProductos();
-        int contadorTrue = 0;
-        int contadorFalse = 0;
-        if (!producto.mapaVacio()) {
-            System.out.println("PRODUCTOS QUE NUNCA HAN TENIDO ENTRADAS:");
-            for (Producto producto : productos.values()) {
-                if (producto.getContadorEntradas() == 0) {
-                    System.out.println("EL PRODUCTO CON CODIGO: " + producto.getCodigo());
-                    System.out.println("===========================");
-                    contadorTrue++;
-                }
-                else{
-                    contadorFalse++;
-                }
-            }
-            if (contadorFalse > 0 && contadorTrue == 0) {
-                System.out.println("NO HAY PRODUCTOS QUE NUNCA HAN TENIDO ENTRADAS");
-            }
-        }
-        else{
-            System.err.println("NO HAY PRODUCTOS REGISTRADOS");
-        }
+        movimientos.productosSinEntradas();
     }
 
     // ============= CASE 5: MOSTRAR PRODUCTOS QUE NUNCA HAN TENIDO SALIDAS ============ 
     public void productosSinSalidas(){
-        Map<String,Producto> productos = producto.getProductos();
-        int contadorTrue = 0;
-        int contadorFalse = 0;
-        if (!producto.mapaVacio()) {
-            System.out.println("PRODUCTOS QUE NUNCA HAN TENIDO SALIDAS:");
-            for (Producto producto : productos.values()) {
-                if (producto.getContadorSalidas() == 0) {
-                    System.out.println("EL PRODUCTO CON CODIGO: " + producto.getCodigo());
-                    System.out.println("===========================");
-                    contadorTrue++;
-                }
-                else{
-                    contadorFalse++;
-                }
-            }
-            if (contadorFalse > 0 && contadorTrue == 0) {
-                System.out.println("NO HAY PRODUCTOS QUE NUNCA HAN TENIDO SALIDAS");
-            }
-        }
-        else{
-            System.err.println("NO HAY PRODUCTOS REGISTRADOS");
-        }
+        movimientos.productosSinSalidas();
     }
 }
